edbfe3005259285fdde59f4dc3b1807989498bbf
Biruk Zerihun <birukz516@gmail.com>
2024-05-25 05:24:17 -0700

    starting my api + length

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,25 @@
 const express = require("express")
-
+const fs = require("fs")
 const app = express()
 
-app.get("/", (req, res) => {
+// app.get("/", (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: "hello from the server side", app: "Music Player" })
+// })
+
+// app.post("/", (req, res) => {
+//   res.send("you can post to this end point")
+// })
+
+const musics = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/musics-sample.json`)
+)
+
+app.get("/api/v1/musics", (req, res) => {
   res
     .status(200)
-    .json({ message: "hello from the server side", app: "Music Player" })
-})
-
-app.post("/", (req, res) => {
-  res.send("you can post to this end point")
+    .json({ status: "success", results: musics.length, data: { musics } })
 })
 
 const port = 3000
