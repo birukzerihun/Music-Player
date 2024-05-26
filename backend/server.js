e5f8455c7a2fc661296aef22e75f0fde297cc3ac
Biruk Zerihun <birukz516@gmail.com>
2024-05-26 01:21:50 -0700

    create simple music model

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,31 @@
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
+const app = require('./app');
 
 const mongoose = require('mongoose');
 mongoose
   .connect(process.env.DATABASE_LOCAL)
   .then(() => console.log('Database connected successfully 🚀'));
 
-const app = require('./app');
+const musicSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'A music should have a title'],
+  },
+  artist: {
+    type: String,
+    required: [true, 'A music should have a artist'],
+  },
+  album: {
+    type: String,
+  },
+  genre: {
+    type: String,
+    required: [true, 'A music should have a genre'],
+  },
+});
+
+const Music = mongoose.model('Music', musicSchema);
 
 const port = process.env.PORT || 3000;
 
