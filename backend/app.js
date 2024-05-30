8ab5367c4eb86c8315ca2e648765b4fd5d48494f
Biruk Zerihun <birukz516@gmail.com>
2024-05-30 11:43:40 -0700

    handling unhandled routes

--- backend/app.js
+++ backend/app.js
@@ -15,16 +15,18 @@ app.use(express.json());
 // app.use(express.static(`${__dirname}/public}`))
 
 app.use((req, res, next) => {
-  console.log('hello from middleware');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requastTime = new Date().toISOString();
   next();
 });
 
 //3) ROUTES
 app.use('/api/v1/musics', musicRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
