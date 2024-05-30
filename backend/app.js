136f630b5407436976ebb515de7db54de54c97d5
Biruk Zerihun <birukz516@gmail.com>
2024-05-30 11:51:29 -0700

    global error handling middleware

--- backend/app.js
+++ backend/app.js
@@ -23,9 +23,24 @@ app.use((req, res, next) => {
 app.use('/api/v1/musics', musicRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`,
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server!`,
+  // });
+
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.statusCode = 404;
+  err.status = 'fail';
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
