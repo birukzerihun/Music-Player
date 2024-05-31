8f1e1044df0779a39971ee3dd1c5407a2e8a03d3
Biruk Zerihun <birukz516@gmail.com>
2024-05-31 00:15:43 -0700

    unhandled rejection

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,14 @@ mongoose
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log('app listening on port ' + port);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION 💥 Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
